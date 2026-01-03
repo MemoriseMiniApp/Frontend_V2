0b45d7b158cffa73f0d3ae13a30a52509ba264da
Bulovyatov <bam10112002@gmail.com>
2026-01-04 00:00:24 +0300

    t

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import './App.css';
 import { useLogin } from './services/LoginProvider';
 
 const App = () => {
-
   const { login } = useLogin();
-  const jwt = login.jwt;
+  const jwt = login?.jwt; // безопасно, если login undefined
 
-  const webApp = window.Telegram.WebApp;
-  webApp.ready();
+  const webApp = window?.Telegram?.WebApp;
+  webApp?.ready(); // вызываем только если webApp существует
 
-  const initData = webApp.initData;
+  const initData = webApp?.initData; // безопасно, если webApp undefined
 
   return (
     <div>
       <p>Hello, it`s Working, hmmm cool</p>
-      <p>{ webApp.initData}</p>
-      <p>{jwt}</p>
+      <p>{initData ?? "Init data not available"}</p> {/* fallback текст если undefined */}
+      <p>{jwt ?? "JWT not available"}</p> {/* fallback текст если undefined */}
     </div>
   );
 };
