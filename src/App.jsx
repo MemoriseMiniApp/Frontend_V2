ca42bc24914f3edf959171756c23b7395446f038
Bulovyatov <bam10112002@gmail.com>
2026-01-04 00:09:48 +0300

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { useEffect, useState } from 'react';
 import { useLogin } from './services/LoginProvider';
 
+import { initMiniApp, retrieveLaunchParams } from '@tma.js/sdk';
+
+
 const App = () => {
   const { login } = useLogin();
   const jwt = login?.jwt;
+
+  const [user, setUser] = useState(null); // Состояние для хранения info о пользователе
+
+
+  useEffect(() => {
+    const [miniApp] = initMiniApp();
+
+    miniApp.ready().then(() => {
+      const { initDataRaw, initData } = retrieveLaunchParams();
+
+      console.log('Raw initData:', initDataRaw);
+      console.log('Parsed initData obj:', initData);
+    });
+  }, []);
 
   const [initData, setInitData] = useState(null);
 
@@ -17,11 +34,13 @@ const App = () => {
     setInitData(webApp.initData);
   }, []);
 
+    if (!user) return <div>Загрузка данных Telegram...</div>;
+
+
   return (
     <div>
-      <p>Hello, it`s Working, hmmm cool</p>
-      <p>{window?.Telegram?.WebApp ?? "Init data not available"}</p>
-      <p>{jwt ?? "JWT not available"}</p>
+      <h1>Привет, {user.first_name}!</h1>
+      <p>Твой Telegram ID: {user.id}</p>
     </div>
   );
 };
