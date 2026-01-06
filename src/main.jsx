c7a80b64ac3a26f65b851096c13dc41e53e6efdb
Bulovyatov <bam10112002@gmail.com>
2026-01-06 21:18:40 +0300

    -

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './styles/gallery.css';
 import { AlbumsProvider } from './context/AlbumsContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
